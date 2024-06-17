388f707e8b9b6873e258deec85a7d5bca28db891
3manuel <101729406+3manuel0@users.noreply.github.com>
2024-06-17 17:58:30 +0100

    Update 2dscript.sh

--- 2dscript.sh
+++ 2dscript.sh
@@ -1,2 +1,1 @@
-gcc 2dGame.c 2D.c -o 2dGame -O1 -Wall -std=c99 -Wno-missing-braces -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-./2dGame
+gcc 2dGame.c 2D.c -o 2dGame -O1 -Wall -std=c99 -Wno-missing-braces -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 && ./2dGame 
